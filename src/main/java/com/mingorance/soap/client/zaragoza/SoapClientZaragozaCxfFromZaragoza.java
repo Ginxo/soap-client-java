be3a3855c7a40df058dd2ec9f1b31b1cf1a61195
Enrique Mingorance <enrique.mingorance.cano@everis.com>
2018-08-06 12:15:06 +0200

    assembly

--- src/main/java/com/mingorance/soap/client/zaragoza/SoapClientZaragozaCxfFromZaragoza.java
+++ src/main/java/com/mingorance/soap/client/zaragoza/SoapClientZaragozaCxfFromZaragoza.java
@@ -14,7 +14,7 @@ public class SoapClientZaragozaCxfFromZaragoza {
 //        System.setProperty("https.proxyPort", "8080");
 //
         AxisProperties.getProperties().put("proxySet","true");
-        AxisProperties.setProperty("http.proxyHost", "10.129.8.100");
+        AxisProperties.setProperty("http.proxyHost", "cache.bancsabadell.com");
         AxisProperties.setProperty("http.proxyPort", "8080");
 
         ImplLoginProxy implLoginServiceProxy = new ImplLoginProxy("https://www.czidconnect.es:8443/messenger/services/ImplLogin");
@@ -23,7 +23,7 @@ public class SoapClientZaragozaCxfFromZaragoza {
         beanLogin.setCad1("1234GGG");
         beanLogin.setCad2("");
         beanLogin.setMensaje("");
-        beanLogin.setPass("PASSS");
+        beanLogin.setPass("PASS");
         beanLogin.setStatus(true);
         beanLogin.setTipo("json/M");
         beanLogin.setUsuario("USER");
